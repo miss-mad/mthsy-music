c3163f8b5299c0503e530995627f0b5ce97c736b
miss-mad <mmlh.harris@gmail.com>
2022-08-18 16:35:27 -0400

    committing to pull from main to start today

--- assets/js/script.lastfm.js
+++ assets/js/script.lastfm.js
@@ -188,10 +188,14 @@ function displaySongTitleSearch(data) {
 artistButton.on("click", getUserInputArtistName);
 titleButton.on("click", getUserInputSongTitleSearch);
 
+// merge all APIs on one js file
+
 // single variable for implementing into the youtube API
 // var firstSongForYoutubeAPI = data.toptracks.track[0].name;
 // insert plus signs into the spaces
+// if () {
 // 
+// }
 
 // single variable for implementing into the ticketmaster API
 // var artistNameForTicketMasterAPI = data.results.trackmatches.track[0].artist;
