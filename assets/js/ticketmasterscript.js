babb550800e2222f993d884ec8a93235d1bb63f9
TeryKing <tery_x3@hotmail.com>
2022-08-18 18:43:15 -0500

    Ticketmaster JS is complete, now merging with LASTFM api from MH

--- assets/js/ticketmasterscript.js
+++ assets/js/ticketmasterscript.js
@@ -1,4 +1,10 @@
 var artistsearch = document.getElementById("artistbuttons");
+var concertdisplay = document.getElementById("concertevent");
+var concertdate = document.getElementById("concertdate");
+var concertname = document.getElementById("concertname");
+var venuename = document.getElementById("venuename");
+var concerturl = document.getElementById("concerturl");
+
 
 var Ticketmaster = function ()
 {
@@ -11,13 +17,41 @@ var url = TicketUrlSearch + UserInput + TicketUrlAPI + TicketAPIKey;
 
     fetch(url)
     .then(response => {
-        return response.json();
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText);
     })
-    
-    console.log(UserInput);
-    console.log(url);
-    console.log("yo");
+    .then(function(data)
+    {
+        console.log(data._embedded.events[0]);
+        console.log(data._embedded.events[0].name);
+        console.log(data._embedded.events[0].dates.start.localDate);
+        console.log(data._embedded.events[0].products[0].name);
+        console.log(data._embedded.events[0]._embedded.venues[0].name);
+        console.log(data._embedded.events[0].url);
 
+
+        var eventname = data._embedded.events[0].name;
+        var eventdate = data._embedded.events[0].dates.start.localDate;
+        var eventconcertname = data._embedded.events[0].products[0].name;
+        var placename = data._embedded.events[0]._embedded.venues[0].name;
+        var eventurl = data._embedded.events[0].url;
+
+        concertdisplay.textContent += eventname;
+        concertdate.textContent += eventdate;
+        concertname.textContent += eventconcertname;
+        venuename.textContent += placename;
+        concerturl.textContent += eventurl;
+   
+
+    })
+    .catch(function(error)
+    {
+        console.log("Error: ",error);
+    })
+
+    
 }
 
 artistsearch.addEventListener("click",Ticketmaster);
